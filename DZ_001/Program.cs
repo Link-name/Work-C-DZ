3bfb7ed19df01f1e3e9a70817d06f89fae798c6b
Link <stereo_house01@mail.ru>
2022-09-16 23:16:30 +0300

    Программа определяющая из 2-х чисел какое больше, а какое меньше

---
+++ DZ_001/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Напишите два числа");
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine("Большим числом является  " + number1);
+    Console.WriteLine("Наименьше число является  " + number2);
+}
+else if (number2 > number1)
+{
+    Console.WriteLine("Наибольше число является " + number2);
+    Console.WriteLine("Наименьше число является " + number1);
+}
+else 
+{
+    Console.WriteLine( "Принятые числа равны равны"); // Немного не допонял задание и сделал программу работающую под любые числа. ( не знаю будет это за ошибку или нет)
+}

b651ade2a7e0f5b2e4deca3c47232392e41575b6
Link <stereo_house01@mail.ru>
2022-09-16 13:12:26 +0300

    Начальная разборка команда ввода строк и переменных

---
+++ work001/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int number = 40 - 9; // int  любое целое число 999 999
+Console.WriteLine("Сколько максиму лет   " + number + number + number);
+string max = number.ToString();
+
+
+Console.WriteLine("Enter name pliz ");
+string name = Console.ReadLine(); // Отдает все строкой 
+Console.WriteLine("Привет, " + name);
+Console.WriteLine("Сколько вам лет ");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine("Вам  " + age + "  лет");
+
+
+Console.WriteLine("Введите число а");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число б ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Ура все правильно!!!!!  =)  " + (a+b));
+
+
+

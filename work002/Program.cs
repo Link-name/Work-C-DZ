a2299b73732fb7c2a86f4340f97cfa5ba02f13a4
Link <stereo_house01@mail.ru>
2022-09-16 13:56:42 +0300

    Изучение if  и  else  ( это команда выполнения условий)

---
+++ work002/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Напишите число a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Напишите число b");
+int b = int.Parse(Console.ReadLine());
+
+
+if (a > b)
+{
+    Console.WriteLine("Число a больше числа b");
+}
+else if (a < b )
+{
+ 
+    Console.WriteLine("Число b больше числа a");
+}
+else
+{
+    Console.WriteLine("Число b равно числу a");
+}

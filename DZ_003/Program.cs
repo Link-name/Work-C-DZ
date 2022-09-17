103a9d129e99ed414309bc1ff2773254c4b8fa2f
Link <stereo_house01@mail.ru>
2022-09-17 12:02:37 +0300

    Сделал программу определяющая четное ли число каторое ввели

---
+++ DZ_003/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Напишите любое число и мы скажем четное или оно");
+int number = int.Parse(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine( number + "   Это число является четным");
+}
+else 
+{
+    Console.WriteLine( "   Увы введенное число не является четным");
+}

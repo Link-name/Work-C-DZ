7966f82f1d069a27c5f1013c39100d7038b3e2e1
Link <stereo_house01@mail.ru>
2022-09-17 11:28:27 +0300

    Сделали программу которая ищет максимальное число из 3-х чисел

---
+++ DZ_002/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Пожалуйста введите 3 любых числа");
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+int number3 = int.Parse(Console.ReadLine());
+int Max = 0;
+if (number1 > number2)
+{
+    Max = number1;
+}
+else
+{
+    Max = number2;
+}
+if (Max > number3)
+{
+    Console.WriteLine("Максимальное число будет   " + Max);
+}
+else
+{
+    Max = number3;
+    Console.WriteLine("Максимальным числом будет  " + Max);
+} 

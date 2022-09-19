39f6a7b0a66023c018fc038b4bd15bb4a6c7d171
Link <stereo_house01@mail.ru>
2022-09-19 13:13:22 +0300

    программа которая показывает все четные числа от 1 до N

---
+++ DZ_004/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите любое число");
+int number = int.Parse(Console.ReadLine());
+int number2 = 1;
+while (number2 <= number)
+{
+
+    if (number2 % 2 == 0)
+    {
+        Console.Write( number2 + " " );
+        number2++;
+    }
+    else
+    {
+        number2++;
+    }
+}
